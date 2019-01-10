efe7c00bf21a087ade1e4a2408d3873edc649ae8
maxjamin <maxjamin@gmail.com>
2019-01-10 10:45:40 -0700

    paragraph

--- web/main.php
+++ web/main.php
@@ -13,7 +13,7 @@
 
 <div class="navbar">
   <a href="main.php">Home</a>
-  <a href="pageTwo.php">News</a>
+  <a href="pageTwo.php">Fixing</a>
   <div class="dropdown">
     <button class="dropbtn">Assignments 
       <i class="fa fa-caret-down"></i>
@@ -28,7 +28,8 @@
 <br>
 <img src="bike01.jpeg" alt="Icon" class="picture01">
 
-<p>Hello, and welcome to bikeriders. Your one stop place for finding places to get out and ride. We also strive to help you fix any bike related problem that you might have. If you have any questions please send us an email and we'll get right back to you.</p> 
+<p>Hello, and welcome to bikeriders. We have been in business for over 10 years and your one stop place for finding places to get out and ride. Whether your in a city or in the middle of nowhere, we hope that you will use our map to plan your next adventure. If your in the dirt or on the rode you might find some new and exciting places to ride. We also strive to help you fix any bike related problem that you might have. Please check out our Fix it page at the top of our web site. If you have any questions please send us an email and we'll get right back to you.</p> 
+
 
 
 
