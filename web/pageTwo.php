a2e9640aaef35689b6bdcc57343d13ac2a7c54ae
maxjamin <maxjamin@gmail.com>
2019-01-10 12:10:39 -0700

    working on fixing bike page

--- web/pageTwo.php
+++ web/pageTwo.php
@@ -24,9 +24,10 @@
   </div> 
 </div>
 
-<object width="420" height="315"
+<embed width="420" height="315"
 data="https://www.youtube.com/watch?v=enG_XbSDrqg">
-</object>
+</embed>
+
 <br>
 <iframe width="420" height="315"
 src="https://www.youtube.com/watch?v=Bbk5RcH0bbQ">
